7ddcea735aad1eb46cfc47001f69a447ae41629e
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-11 17:29:35 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def initial_round
   # code #initial_round here
   cardOne = deal_card
   cardTwo = deal_card
-  display_card_total
+  display_card_total(cardOne + cardTwo)
 end
 
 def hit?(current_card_total)
