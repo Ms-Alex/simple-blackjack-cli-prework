5382f7caa96b0b5baa37a065b70c47ec6eca69c5
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-11 17:38:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,7 +44,7 @@ def hit?(current_card_total)
   if user == 'h'
     new_card = deal_card
     current_card_total += new_card
-  elsif user != 's' || user != 'h'
+  elsif user != 'h' || user != 's'
     invalid_command
     prompt_user
   end
