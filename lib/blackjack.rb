46cf0814ba563646da5f14f6eb3b8f27df13e48e
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-11 17:33:23 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -45,7 +45,7 @@ def hit?(current_card_total)
     new_card = deal_card
     current_card_total += new_card
   elsif user == 's'
-    puts "You decided to stay."
+    prompt_user
   else
     invalid_command
     prompt_user
